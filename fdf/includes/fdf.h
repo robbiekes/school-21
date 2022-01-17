074493f45ea92496e6c45d752a04c60a4a697297
Myrtice Gwynesse <mgwyness@at-e1.msk.21-school.ru>
2022-01-17 21:06:16 +0300

    map is displayed

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -1,16 +1,29 @@
 #ifndef FDF_H
 #define FDF_H
-
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 #include <mlx.h>
 #include <math.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
 
+typedef struct s_point
+{
+	float	x;
+	float  	y;
+}			t_point;
+
 typedef struct s_map
 {
 	int		height;
 	int		width;
+	int		zoom;
+	int		**z_matrix;
+	int		shift_x;
+	int		shift_y;
+	void	*mlx_ptr;
+	void	*win_ptr;
 } 			t_map;
 
 typedef struct s_data
@@ -29,11 +42,11 @@ int			ft_ptrlen(char **s);
 
 ////////in use////////
 void		finish(int exitcode);
-void	read_map(char *map, t_map *map_data);
-int	get_height(char *file);
-int	get_width(char *file);
-void	free_splitted_exit(char **arr);
-char	*get_next_line(int fd);
+void		read_map(char *map, t_map *map_data);
+int			get_height(char *file);
+int			get_width(char *file);
+void		free_splitted_exit(char **arr, int flag);
+char		*get_next_line(int fd);
 
 
 
--- fdf/srcs/fdf.c
+++ fdf/srcs/fdf.c
@@ -6,11 +6,12 @@
 /*   By: mgwyness <mgwyness@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 15:38:12 by mgwyness          #+#    #+#             */
-/*   Updated: 2022/01/16 20:33:43 by mgwyness         ###   ########.fr       */
+/*   Updated: 2022/01/17 20:20:40 by mgwyness         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <unistd.h>
 ///////////////put pixel on a screen//////////////////////
 void	my_mlx_pixel_put(t_data *data, int x, int y, int colour)
 {
@@ -19,38 +20,51 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int colour)
 	dst = data->address + (y * data->line_len + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = colour;
 }
-/////////////////draw a line///////////////////////
-void	draw_line(int x, int y, int x1, int y1, t_data *img)
+
+int	key_num(int	key, void *data)
 {
-	int	deltaX = abs(x1 - x);
-	int	deltaY = abs(y1 - y);
-	int signX = 0;
-	int signY = 0;
-	if (x < x1)
-		signX = 1;
-	else
-		signX = -1;
-	if (y < y1)
-		signY = 1;
-	else
-		signY = -1;
-	int error = deltaX - deltaY;
-	my_mlx_pixel_put(img, x1, y1, 0x00FF0000);
-	while(x != x1 || y != y1) 
+	printf("%d\n", key);
+	return (0);
+}
+
+void	isometry(t_point *p, int z)
+{
+	p->x = (p->x - p->y) * cos(0.8);
+	p->y = (p->x + p->y) * sin(0.8) - z;
+}
+
+/////////////////draw a line///////////////////////
+void	draw_line(t_point *p, t_point *p1, t_data *data, int colour)
+{
+	float	x_step;
+	float	y_step;
+	int		max;
+	
+	// int	z;
+	// int	z1;
+
+	// z = map_data->z_matrix[(int)p1->y][(int)p1->x];
+	// z1 = map_data->z_matrix[(int)p2->y][(int)p2->x];
+	// x *= map_data->zoom;
+	// y *= map_data->zoom;
+	// x1 *= map_data->zoom;
+	// y1 *= map_data->zoom;
+
+	x_step = p1->x - p->x;
+	y_step = p1->y - p->y;
+
+	max = MAX(ABS(x_step), ABS(y_step));
+	x_step /= max;
+	y_step /= max;
+	while ((int)(p->x - p1->x) || (int)(p->y - p1->y))
 	{
-		my_mlx_pixel_put(img, x, y, 0x00FF0000);
-		int error2 = error * 2;
-		if(error2 > -deltaY) 
-		{
-			error -= deltaY;
-			x += signX;
-		}
-		if(error2 < deltaX) 
-		{
-			error += deltaX;
-			y += signY;
-		}
+		my_mlx_pixel_put(data, (int)p->x, (int)p->y, colour);
+		p->x += x_step;
+		p->y += y_step;
 	}
+	//isometry(&x, &y, z);
+	//isometry(&x1, &y1, z);
+
 }
 ////////////count height of the map/////////////////
 int	get_height(char *file)
@@ -72,7 +86,7 @@ int	get_height(char *file)
 	return (strings);
 }
 ////////free splitted string and terminate the whole program/////////
-void	free_splitted_exit(char **arr)
+void	free_splitted_exit(char **arr, int flag)
 {
 	int	i;
 
@@ -80,72 +94,172 @@ void	free_splitted_exit(char **arr)
 	while (arr[i])
 			free(arr[i++]);
 	free(arr);
-	finish(0);
+	if (flag)
+		finish(0);
 }
 
 ////////////get width of the map///////////////
 int	get_width(char *file)
 {
-	int		string_len;
 	int		fd;
 	char	*string;
 	char	**splitted_str;
 	
 	fd = open(file, O_RDONLY, 0);
+	if (fd == -1)
+		return (0);
 	string = get_next_line(fd);
 	if (string)
 	{
 		splitted_str = ft_split(string, ' ');
+		if (!splitted_str)
+			finish(0);
 		free(string);
-		if (!splitted_str)
-			finish(0);
-		string_len = ft_ptrlen(splitted_str);
 	}
-	string = get_next_line(fd);
-	while (string)
-	{
-		splitted_str = ft_split(string, ' ');
-		if (!splitted_str)
-			finish(0);
-		if (ft_ptrlen(splitted_str) != string_len)
-		{
-			write(1, "error\n", 6);
-			free(string);
-			finish(0);
-			// free_splitted_exit(splitted_str);
-		}
-		 free(string);
-		string = get_next_line(fd);
-	}
-	// close (fd);
-	return (string_len);
+	return (ft_ptrlen(splitted_str));
 }
+
+void	fill_matrix(t_map *map_data, char *file)
+{
+	int		fd;
+	int		i;
+	int		j;
+	char	*string;
+	char	**splitted_str;
+
+	fd = open(file, O_RDONLY, 0);
+	i = 0;
+	j = 0;
+	while (i < map_data->height)
+	{
+		string = get_next_line(fd);
+		splitted_str = ft_split(string, ' ');
+		j = 0;
+		while (j < map_data->width)
+		{
+			map_data->z_matrix[i][j] = ft_atoi(splitted_str[j]);
+			j++;
+		}
+		free_splitted_exit(splitted_str, 0);
+		free(string);
+		i++;
+	}
+	i = 0;
+	// while (i < map_data->height)
+	// {
+	// 	j = 0;
+	// 	while (j < map_data->width)
+	// 	{
+	// 		printf("%3d", map_data->z_matrix[i][j]);
+	// 		j++;
+	// 	}
+	// 	printf("\n");
+	// 	i++;
+	// }
+}
+
 ////////////////read a map/////////////////
 void	read_map(char *map, t_map *map_data)
 {
-	printf("height = %d width = %d\n", get_height(map), get_width(map));
+	int	i;
+	
+	i = 0;
+	if (!get_height(map) || !get_width(map))
+	{
+		free(map_data);
+		exit (0);
+	}
+	map_data->height = get_height(map);
+	map_data->width = get_width(map);
+
+	if (!(map_data->z_matrix = (int **)malloc(sizeof(int *) * map_data->height)))
+		exit (0);
+	while (i < map_data->height)
+		map_data->z_matrix[i++] = (int *)malloc(sizeof(int) * map_data->width); // check malloc
+
+	fill_matrix(map_data, map);
+	// printf("height = %d width = %d\n", map_data->height, map_data->width);
+}
+
+void	shift_dot(t_point *p, t_map *map_data)
+{
+	p->x += map_data->shift_x;
+	p->y += map_data->shift_y;
+}
+
+void	draw_map(t_map *map_data, t_data *data)
+{
+	t_point	p;
+	t_point p1;
+	int		x;
+	int		y;
+
+	x = 0;
+	y = 0;
+	while (y < map_data->height)
+	{
+		x = 0;
+		while (x < map_data->width)
+		{
+			p.x = x * map_data->zoom;
+			p.y = y * map_data->zoom;
+			// p.x = 100;
+			// p.y = 100;
+			// p1.x = 200;
+			// p1.y = 200;
+			// draw_line(&p, &p1, data, 0x00FFFFFF);
+			// return ;
+			if (x < map_data->width - 1)
+			{
+				p1.x = (x + 1) * map_data->zoom;
+				p1.y = y * map_data->zoom;
+				isometry(&p, map_data->z_matrix[y][x]);
+				isometry(&p1, map_data->z_matrix[y][x + 1]);
+				shift_dot(&p, map_data);
+				shift_dot(&p1, map_data);
+				draw_line(&p, &p1, data, 0x00FFFFFF);
+			}
+			if (y < map_data->height - 1)
+			{
+				p.x = x * map_data->zoom;
+				p.y = y * map_data->zoom;
+				p1.x = x * map_data->zoom;
+				p1.y = (y + 1) * map_data->zoom;
+				isometry(&p, map_data->z_matrix[y][x]);
+				isometry(&p1, map_data->z_matrix[y + 1][x]);
+				shift_dot(&p, map_data);
+				shift_dot(&p1, map_data);
+				draw_line(&p, &p1, data, 0x00FFFFFF);
+			}
+			x++;
+		}
+		write(1, "\n", 1);
+		y++;
+	}
 }
 
 int main(int argc, char **argv)
 {
 	t_data	img;
 	t_map	*map_data;
-	void	*mlx;
-	void	*mlx_window;
 
 	map_data = (t_map *)malloc(sizeof(t_map));
 	read_map(argv[1], map_data);
-	mlx = mlx_init();
-	mlx_window = mlx_new_window(mlx, 1000, 1000, "FDF");
-	img.image = mlx_new_image(mlx, 1000, 1000);
+	map_data->mlx_ptr = mlx_init();
+	map_data->win_ptr = mlx_new_window(map_data->mlx_ptr, 1000, 1000, "FDF");
+	img.image = mlx_new_image(map_data->mlx_ptr, 1000, 1000);
 	img.address = mlx_get_data_addr(img.image, &img.bits_per_pixel,
 					&img.line_len, &img.endian);
-	
+	map_data->zoom = 20;
+	map_data->shift_x = 600;
+	map_data->shift_y = 100;
+	draw_map(map_data, &img);
+	mlx_key_hook(map_data->win_ptr, key_num, 0);
 	// draw_circle(300, 300, 140, 0x00FF0000, &img);
 	// draw_square(&img, &mlx, &mlx_window);
 	// draw_circle(500, 300, 138, 0x000000FF, &img);
 	// draw_circle(400, 400, 136, 0x00FF0000, &img);
 	// draw_line(50, 50, 200, 150, &img);
-	// mlx_put_image_to_window(mlx, mlx_window, img.image, 0, 0);
-	// mlx_loop(mlx);
+	mlx_put_image_to_window(map_data->mlx_ptr, map_data->win_ptr, img.image, 0, 0);
+	mlx_loop(map_data->mlx_ptr);
 } 
