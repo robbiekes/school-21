81de9e7a6b6ff53cca5fa8b33f1d8803896fe0cb
robbiekes <zaira.azhen@gmail.com>
2022-02-03 18:57:56 +0300

    new funcs

--- pipex/srcs/pipe.c
+++ pipex/srcs/pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgwyness <mgwyness@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zaira <zaira@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 17:55:28 by mgwyness          #+#    #+#             */
-/*   Updated: 2022/02/02 19:54:28 by mgwyness         ###   ########.fr       */
+/*   Updated: 2022/02/03 18:55:44 by zaira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,56 @@
 // {
 	
 // }
+int ffork()
+{
+	pid_t	id;
+	id = fork();
+	if (id == -1)
+	{
+		perror("fork error");
+		return (0);	
+	}
+	else if (id == 0)
+		printf("child process  created\n");
+	else
+		printf("parent process after work\n");
+	return (id);
+}
 
-int	main()
+int	wwaitpid()
+{
+	pid_t	child_pid;
+	pid_t	pid;
+	int	status;
+	pid = 0;
+	status = 0;
+	child_pid = ffork();
+	pid = waitpid(child_pid, &status, WUNTRACED);
+	printf("[%d]\n", child_pid);
+	if (WIFEXITED(status))
+	{
+		write(2, &child_pid, 1);
+		printf("exits: %d\n", WEXITSTATUS(status));
+	}
+	else if (WIFSTOPPED(status))
+	{
+		write(2, &child_pid, 1);
+		printf("stopped by: %d\n", WSTOPSIG(status));
+	}
+	else if (WIFSIGNALED(status))
+	{
+		write(2, &child_pid, 1);
+		printf("killed by: %d\n", WTERMSIG(status));
+	}
+	else
+	{
+		perror("waitpid error\n");
+		exit(0);
+	}
+	return (0);
+}
+
+int	ppipe()
 {
 	pid_t	child_pid;
 	int		fifo[2];
@@ -50,4 +98,39 @@ int	main()
 	// if (waitpid(child_pid, &status, 0) == child_pid && WIFEXITED(status))
 	// 	return (WEXITSTATUS(status));
 	return (3);
+}
+
+int	redirection()
+{
+	int	fd;
+	int	fd2;
+
+	if (fork())
+	{
+		fd = open("src_file", O_RDONLY);
+		fd2 = open("dst_file", O_WRONLY, O_CREAT, O_TRUNC);
+		if (fd == -1 || fd2 == -1)
+		{
+			perror("open error");
+			exit(1);
+		}
+		dup2(fd, STDIN_FILENO);
+		if (dup2 == -1)
+			exit(5);
+		dup2(fd2, STDOUT_FILENO);
+		if (dup2 == -1)
+			exit(6);
+		close(fd);
+		close(fd2);
+		execlp("sort", "sort", 0);
+		exit(8);
+	}
+	return (0);
+}
+
+int main()
+{
+	// ffork();
+	// ppipe();
+	wwaitpid();
 }
