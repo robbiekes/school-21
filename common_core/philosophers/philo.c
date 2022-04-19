f1590f072f8e4bf657f5c658b26f16f34dbf2171
Myrtice Gwynesse <mgwyness@student.21-school.ru>
2022-04-19 22:17:30 +0300

    some changes in structures

--- common_core/philosophers/philo.c
+++ common_core/philosophers/philo.c
@@ -1,34 +1,96 @@
 #include "philo.h"
 
-// написать ft_atoi()
+// TODO: add ft_atoi()
 
-void	init_args(t_data *data, char **av)
+int	init_phil_data(t_philo **philo, int ac, char **av)
 {
-	data = (t_data *)malloc(sizeof(t_data));
-	data->philo_num = ft_atoi(av[0]);
-	data->time_to_die = ft_atoi(av[1]);
-	data->time_to_eat = ft_atoi(av[2]);
-	data->time_to_sleep = ft_atoi(av[3]);
+	*philo = (t_philo *)malloc(sizeof(t_philo));
+	if (!*philo)
+		return (1);
+	(*philo)->philo_num = ft_atoi(av[1]);
+	if ((*philo)->philo_num < 1) // ONE OR MORE philosophers sit at a round table
+	{
+		free(*philo);
+		return (1);
+	}
+	(*philo)->time_to_die = ft_atoi(av[2]);
+	(*philo)->time_to_eat = ft_atoi(av[3]);
+	(*philo)->time_to_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		(*philo)->each_must_eat = ft_atoi(av[5]);
+	(*philo)->must_die = 0;
+	return (0);
 }
 
-void	init_philos(t_data *data);
 
-void	init_threads(t_data *data);
+int	init_threads(t_philo *philo, p_data *data)
+{
+	int			i;
+	pthread_t	id;
+	pthread_t	*thread_ids;
 
-void	init_mutex(t_data *data);
+
+	i = -1;
+	thread_ids = (pthread_t *)malloc(sizeof(pthread_t) * philo->philo_num);
+	if (!thread_ids)
+		return (1);
+	while (++i < philo->philo_num)
+		thread_ids[i] = 0;
+	i = -1;
+	while (++i < philo->philo_num)
+	{
+		if (pthread_create(&id, 0, philosophers, philo))
+		{
+			i = -1;
+			philo->must_die = 1;
+			while (thread_ids[++i])
+				pthread_join(thread_ids[i], 0);
+			free(thread_ids);
+			free(philo);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	init_forks(p_data *data) // aka forks (== philo_num)
+{
+	int		i;
+
+	i = -1;
+	data->forks = (p_mutex *)malloc(sizeof(p_mutex));
+	if (!data->forks)
+		return (1);
+	while (++i < data->philo->philo_num)
+		pthread_mutex_init(&(data->forks[i]), 0);
+}
+
+void	*philosophers(void *arg)
+{
+	t_philo *philo = (t_philo *) arg;
+	// if philo-1 and philo+1 didn't eat and 
+}
 
 int main(int ac, char **av)
 {
-	t_data	*data;
+	int		i;
+	t_philo	*philo;
+	p_data	*data;
+	check_args(ac, av);
+	if (init_phil_data(&philo, ac, av))
+		return (1);
 
-	init_args(data, av);
-	//init_philos(data);
-	//init_threads(data);
-	//init_mutex(data);
+	if (init_forks(philo))
+		return (1);
 
-	while (++i < data->philo_num) // проверка на то, не умер ли какой-либо философ
-	// если умер, return (0)
+	if (init_threads(data, philo))
+		return (1);
 
+	i = -1;
+	while (++i < philo->philo_num)
+		pthread_join(&(data->thread_ids[i]), 0);
+
+	free(philo);
 	free(data);
 	return (0);
 	
--- common_core/philosophers/philo.h
+++ common_core/philosophers/philo.h
@@ -5,16 +5,26 @@
 # include <unistd.h>
 # include <pthread.h>
 
-typedef pthread_mutex_t t_mutex;
+typedef pthread_mutex_t p_mutex;
 
-typedef struct s_data 
+typedef struct s_philo 
 {
-	int philo_num; // == num of threads
-	int time_to_eat; //in ms
-	int time_to_die; //in ms
-	int time_to_sleep; //in ms
-	int *small;
-	int *large;
-} t_data;
+	int		philo_num; // == num of threads
+	int 	time_to_eat; //in ms
+	int 	time_to_die; //in ms
+	int 	time_to_sleep; //in ms
+	int		each_must_eat; // how many time each philo must eat
+	int		last_meal; // in ms - when each philo eated last time
+	int		must_die;
+	p_mutex	*left_fork;
+	p_mutex	*right_fork;
+}	t_philo;
+
+typedef struct s_data
+{
+	p_mutex		*forks;
+	pthread_t	*thread_ids;
+	t_philo		*philo;
+}	p_data;
 
 #endif
