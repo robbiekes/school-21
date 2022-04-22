5a5de2a182c047ff2479867d990863ab98b5e48f
robbiekes <zaira.azhen@gmail.com>
2022-04-22 16:16:06 +0300

    algo in process

--- common_core/philosophers/philo.c
+++ common_core/philosophers/philo.c
@@ -6,7 +6,6 @@
 int	init_data(t_data *data, int ac, char **av)
 {
 	data->philo_num = ft_atoi(av[1]);
-	data->must_die = 0;
 	data->forks = (t_fork **)malloc(sizeof(t_fork *) * data->philo_num);
 	if (!(data->forks))
 		return (1);
@@ -40,7 +39,7 @@ int	init_phil_data(t_data *data, int ac, char **av)
 		else
 			data->philos[i]->each_must_eat = -1;
 		data->philos[i]->must_die = 0;
-		// data->philos[i]->write_mutex = 0;
+		//data->philos[i]->write_mutex = 0;
 	}
 	return (0);
 }
@@ -68,22 +67,22 @@ unsigned int	get_time_ms(void)
 	return (cur_time.tv_sec * 1000 + cur_time.tv_usec / 1000);
 }
 
-int	init_threads(t_data *data)
+int	create_threads(t_data *data)
 {
 	int	i;
 
 	i = -1;
 	while (++i < data->philo_num)
 		data->thread_ids[i] = 0;
-	data->philos[i]->start_time = get_time_ms();
 	i = -1;
 	while (++i < data->philo_num)
 	{
+		data->philos[i]->start_time = get_time_ms();
 		data->philos[i]->last_meal = get_time_ms();
 		if (pthread_create(&(data->thread_ids[i]), 0, philosophers, data->philos[i]))
 		{
 			i = -1;
-			data->must_die = 1;
+			data->philos[i]->must_die = 1;
 			while (data->thread_ids[++i])
 				pthread_join(data->thread_ids[i], 0);
 			free_all(data);
@@ -98,16 +97,28 @@ int	init_forks(t_data *data) // aka forks (== philo_num)
 	int		i;
 
 	i = -1;
-	data->forks = (p_mutex *)malloc(sizeof(p_mutex));
-	if (!data->forks)
-		return (1);
-	while (++i < data->philo->philo_num)
-		pthread_mutex_init(&(data->forks[i]), 0);
+	while (++i < data->philo_num)
+	{
+		data->forks[i] = (t_fork *)malloc(sizeof(t_fork));
+		if (!(data->forks[i]))
+			return (1);
+		data->forks[i]->num = i;
+		if (pthread_mutex_init(&(data->forks[i]->mutex), 0))
+			return (1);
+	}
 }
 
 void	*philosophers(void *arg)
 {
-	t_philo *philo = (t_philo *) arg;
+	t_philo	*one_philo;
+	int		i;
+
+	one_philo = (t_philo *)arg;
+	//philo comes to table and sleep() for time_to_sleep ms
+	// 1. take_forks() (== pthread_mutex_lock()) & print_message(eat)
+	// 2. sleep() for time_to_eat ms & print_message(sleep)
+	// 3. put_back_forks() (== pthread_mutex_unlock()) & print_message(put back fork)
+	// 4. sleep() for time_to_eat ms & print_message(sleep)
 	// if philo-1 and philo+1 didn't eat and 
 }
 
@@ -133,16 +144,20 @@ int main(int ac, char **av)
 	
 	if (check_args(ac, av))
 		return (1);
+
 	if (init_data(data, ac, av))
 		return (1);
-	if (init_phil_data(&philo, ac, av))
+
+	if (init_phil_data(data, ac, av))
+		return (1);
+
+	if (init_threads(data))
 		return (1);
 
 	if (init_forks(philo))
 		return (1);
 
-	if (init_threads(data, philo))
-		return (1);
+	
 
 	i = -1;
 	while (++i < philo->philo_num)
